5f622273bc78257676e483b801d0e290ba02c2b2
Damir Dobric <ddobric@daenet.de>
2015-10-26 11:49:10 +0100

    Revert "Revert "Initial move to git.""
    This reverts commit e921e84b2d1a860f90cfc8dbae00514079bcf7ef.

---
+++ DtfWeatherForecast/Program.cs
@@ -0,0 +1,40 @@
+﻿using DurableTask;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtfWeatherForecast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            run();
+        }
+        private static void run()
+        {
+            string connectionString = ConfigurationManager.AppSettings["SbConnStr"];
+            string taskHubName = ConfigurationManager.AppSettings["TaskHubName"];
+
+            TaskHubClient taskHubClient = new TaskHubClient(taskHubName, connectionString);
+            TaskHubWorker taskHubWorker = new TaskHubWorker(taskHubName, connectionString);
+
+            taskHubWorker.CreateHub();
+            OrchestrationInstance instance = taskHubClient.CreateOrchestrationInstance(typeof(WfTaskOrchestration), "Hello");
+
+            taskHubWorker.AddTaskOrchestrations(typeof(WfTaskOrchestration));
+            taskHubWorker.AddTaskActivities(new WOEIDLookupTask(), new WForecastsTask());
+            taskHubWorker.Start();
+
+            Console.WriteLine("Press any key to quit.");
+            Console.ReadLine();
+
+
+            taskHubWorker.Stop(true);
+        }
+
+    }
+}
